1a72c58b0ae8712c9967a30e9d1c42ef40545f9e
SuvStreet <suvstreet@gmail.com>
2017-08-26 15:25:05 +0300

    Update Test.cpp

--- Laboratory_work/Work1/Task1/Test.cpp
+++ Laboratory_work/Work1/Task1/Test.cpp
@@ -1,1 +1,37 @@
-test
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <codecvt>
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	std::ifstream filein;
+	filein.open("text.txt");
+	if (!filein.is_open()) { std::cout << "File not found!" << std::endl; }
+
+	std::wofstream fileout;
+	fileout.open("Ltext.txt");
+	if (!fileout.is_open()) { std::cout << "The file is not created!" << std::endl; }
+
+	std::locale loc(std::locale::classic(), new std::codecvt_utf8<wchar_t>);
+	fileout.imbue(loc);
+
+	std::string str;
+	while (std::getline(filein, str)) {
+		size_t len = mbstowcs(NULL, str.c_str(), 0) + 1;
+		wchar_t* str0 = new wchar_t[len];
+		int count = mbstowcs(str0, str.c_str(), len);
+		fileout << str0;
+		fileout << std::endl;
+		delete[] str0;
+	}
+
+	filein.close();
+	fileout.close();
+
+	return 0;
+}
